b9bc6f73e53d39b5e0e648e4efca1e992eab1085
Lucas Lima Carioca <60993665+Lairu-Fire@users.noreply.github.com>
2025-09-24 09:45:47 -0400

    add index and script

---
+++ script.js
@@ -0,0 +1,17 @@
+var map = L.map('map').setView([-3.042273, -59.975629], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
